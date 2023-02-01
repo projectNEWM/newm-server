b5aa9900f077c43a577c2cf24e519a2396019530
Andrew Westberg <andrewwestberg@gmail.com>
2023-02-01 20:00:42 +0000

    Add additional MintingStatus states

--- newm-server/src/main/kotlin/io/newm/server/features/song/model/MintingStatus.kt
+++ newm-server/src/main/kotlin/io/newm/server/features/song/model/MintingStatus.kt
@@ -1,8 +1,14 @@
 package io.newm.server.features.song.model
 
 enum class MintingStatus {
-    Undistributed,
-    Distributed,
-    Pending,
-    Rejected
+    Undistributed, // Song has been uploaded/created. User may still be editing metadata for it.
+    StreamTokenAgreementApproved, // Artist has approved the Agreement /w placeholder for ISRC number
+    MintingPaymentRequested, // We've created a payment address and presented that to the Artist's wallet for payment.
+    MintingPaymentReceived, // Payment has been received in the payment address and 3 blocks have occurred since.
+    ReadyToDistribute, // Ready for the distribution team to send all the song information to distribution partners.
+    SubmittedForDistribution, // Distribution sets this status once they have uploaded song/metadata for distribution.
+    Distributed, // Distribution partner has successfully deployed to streaming platforms. ISRC number added to song record.
+    Declined, // Distribution partner rejected song for some reason. Manual triage required.
+    Pending, // Song has entered the queue for minting
+    Minted, // Song has completed the minting process and stream tokens are in the Artist wallet.
 }
