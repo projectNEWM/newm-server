d37efdc3aa2f413adf97c4818ea00f28241c5e71
Andrew Westberg <andrewwestberg@gmail.com>
2023-02-15 18:57:01 +0000

    CU-863g0zzgg: submitTx gRPC api

--- newm-chain/src/main/kotlin/io/newm/chain/di/DependencyInjectionInstall.kt
+++ newm-chain/src/main/kotlin/io/newm/chain/di/DependencyInjectionInstall.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.newm.chain.daemon.daemonsKoinModule
 import io.newm.chain.database.databaseKoinModule
+import io.newm.chain.grpc.grpcKoinModule
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.slf4j.LoggerFactory
@@ -19,6 +20,7 @@ fun Application.installDependencyInjection() {
             appKoinModule,
             daemonsKoinModule,
             databaseKoinModule,
+            grpcKoinModule,
         )
     }
 }
---
+++ newm-chain/src/main/kotlin/io/newm/chain/grpc/GrpcKoinModule.kt
@@ -0,0 +1,7 @@
+package io.newm.chain.grpc
+
+import org.koin.dsl.module
+
+val grpcKoinModule = module {
+    single { TxSubmitClientPool(5) }
+}
