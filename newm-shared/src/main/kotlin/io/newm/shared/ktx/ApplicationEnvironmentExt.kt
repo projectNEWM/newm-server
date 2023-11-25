b2595f65a9ac43dfdd26486bf750128b106671cc
Andrew Westberg <andrewwestberg@gmail.com>
2023-11-25 20:59:36 +0000

    Support empty environment lists

--- newm-shared/src/main/kotlin/io/newm/shared/ktx/ApplicationEnvironmentExt.kt
+++ newm-shared/src/main/kotlin/io/newm/shared/ktx/ApplicationEnvironmentExt.kt
@@ -13,7 +13,7 @@ fun ApplicationEnvironment.getConfigString(path: String): String = config.getStr
 fun ApplicationEnvironment.getConfigStrings(path: String): List<String> = config.getStrings(path)
 
 fun ApplicationEnvironment.getConfigSplitStrings(path: String, delimiter: String = ","): List<String> =
-    config.getSplitStrings(path, delimiter)
+    config.getSplitStrings(path, delimiter).filter { it.isNotBlank() }
 
 fun ApplicationEnvironment.getConfigInt(path: String): Int = config.getInt(path)
 
