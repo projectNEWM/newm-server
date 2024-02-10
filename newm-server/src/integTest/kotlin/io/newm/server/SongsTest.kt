00975071c2c3438d417db1502604885ec393b82c
jayvaznewm <124834404+jayvaznewm@users.noreply.github.com>
2024-02-10 14:14:27 -0800

    fix: cleanup song data after integ tests (#376)

--- newm-server/src/integTest/kotlin/io/newm/server/SongsTest.kt
+++ newm-server/src/integTest/kotlin/io/newm/server/SongsTest.kt
@@ -3,6 +3,7 @@ package io.newm.server
 import com.google.common.truth.Truth.assertThat
 import io.ktor.client.call.body
 import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
@@ -30,6 +31,8 @@ class SongsTest {
             assertThat(createSongResponse.status).isEqualTo(HttpStatusCode.OK)
             val resp = createSongResponse.body<SongIdBody>()
             assertThat(resp.songId).isNotNull()
+
+            cleanUpSong(createSongResponse)
         }
 
     @Test
@@ -42,6 +45,8 @@ class SongsTest {
                     )
                 )
             assertThat(createSongResponse.status).isEqualTo(HttpStatusCode.UnprocessableEntity)
+
+            cleanUpSong(createSongResponse)
         }
 
     @Test
@@ -67,6 +72,8 @@ class SongsTest {
             val song = getSongResponse.body<Song>()
             assertThat(song.title).isEqualTo(title)
             assertThat(song.genres).containsExactly("Synthwave", "Synthpop")
+
+            cleanUpSong(createSongResponse)
         }
 
     @Test
@@ -88,4 +95,20 @@ class SongsTest {
             setBody(song)
         }
     }
+
+    private suspend fun deleteSong(songId: String): HttpResponse {
+        return TestContext.client.delete("${TestContext.baseUrl}/v1/songs/$songId") {
+            bearerAuth(TestContext.loginResponse.accessToken)
+            contentType(ContentType.Application.Json)
+        }
+    }
+
+    private suspend fun cleanUpSong(songId: String) {
+        try {
+            deleteSong(songId)
+        } catch (_: Exception) {
+        }
+    }
+
+    private suspend fun cleanUpSong(httpResponse: HttpResponse) = cleanUpSong(httpResponse.body<SongIdBody>().songId.toString())
 }
