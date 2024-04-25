b2717179ab9f798016cb3408b380878bba5ae28f
Saif <smasadeh@newm.io>
2024-04-24 17:37:50 -0700

    minor: code-refactoring-test adding unit test files

--- newm-server/src/main/kotlin/io/newm/server/features/arweave/repo/Util.kt
+++ newm-server/src/main/kotlin/io/newm/server/features/arweave/repo/Util.kt
@@ -27,58 +27,57 @@ object Util {
     private val amazonS3: AmazonS3 by inject()
     private val IMAGE_WEBP_REPLACE_REGEX = Regex("\\.(png|jpg|jpeg|bmp|gif|tiff)\$", RegexOption.IGNORE_CASE)
 
-    suspend fun weaveRequest(
-        song: Song
-    ) = WeaveRequest(
-        json.encodeToString(
-            WeaveProps(
-                arweaveWalletJson = environment.getSecureConfigString("arweave.walletJson"),
-                files =
-                    listOfNotNull(
-                        if (song.arweaveCoverArtUrl != null) {
-                            log.info { "Song ${song.id} already has arweave cover art url: ${song.arweaveCoverArtUrl}" }
-                            null
-                        } else {
-                            song.coverArtUrl.asValidUrl().replace(IMAGE_WEBP_REPLACE_REGEX, ".webp") to "image/webp"
-                        },
-                        if (song.arweaveTokenAgreementUrl != null) {
-                            log.info { "Song ${song.id} already has arweave token agreement url: ${song.arweaveTokenAgreementUrl}" }
-                            null
-                        } else {
-                            song.tokenAgreementUrl!! to "application/pdf"
-                        },
-                        if (song.arweaveClipUrl != null) {
-                            log.info { "Song ${song.id} already has arweave clip url: ${song.arweaveClipUrl}" }
-                            null
-                        } else {
-                            song.clipUrl!! to "audio/mpeg"
-                        },
-                        if (song.arweaveLyricsUrl != null) {
-                            log.info { "Song ${song.id} already has arweave lyrics url: ${song.arweaveLyricsUrl}" }
-                            null
-                        } else {
-                            song.lyricsUrl?.let { it to "text/plain" }
-                        },
-                    ).map { (inputUrl, contentType) ->
-                        val downloadUrl =
-                            if (inputUrl.startsWith("s3://")) {
-                                val (bucket, key) = inputUrl.toBucketAndKey()
-                                amazonS3.generatePresignedUrl(
-                                    bucket,
-                                    key,
-                                    Date.from(Instant.now().plus(30, ChronoUnit.MINUTES)),
-                                    HttpMethod.GET
-                                ).toExternalForm()
+    suspend fun weaveRequest(song: Song) =
+        WeaveRequest(
+            json.encodeToString(
+                WeaveProps(
+                    arweaveWalletJson = environment.getSecureConfigString("arweave.walletJson"),
+                    files =
+                        listOfNotNull(
+                            if (song.arweaveCoverArtUrl != null) {
+                                log.info { "Song ${song.id} already has arweave cover art url: ${song.arweaveCoverArtUrl}" }
+                                null
                             } else {
-                                inputUrl
-                            }
-                        WeaveFile(
-                            url = downloadUrl,
-                            contentType = contentType,
-                        )
-                    },
-                checkAndFund = false,
+                                song.coverArtUrl.asValidUrl().replace(IMAGE_WEBP_REPLACE_REGEX, ".webp") to "image/webp"
+                            },
+                            if (song.arweaveTokenAgreementUrl != null) {
+                                log.info { "Song ${song.id} already has arweave token agreement url: ${song.arweaveTokenAgreementUrl}" }
+                                null
+                            } else {
+                                song.tokenAgreementUrl!! to "application/pdf"
+                            },
+                            if (song.arweaveClipUrl != null) {
+                                log.info { "Song ${song.id} already has arweave clip url: ${song.arweaveClipUrl}" }
+                                null
+                            } else {
+                                song.clipUrl!! to "audio/mpeg"
+                            },
+                            if (song.arweaveLyricsUrl != null) {
+                                log.info { "Song ${song.id} already has arweave lyrics url: ${song.arweaveLyricsUrl}" }
+                                null
+                            } else {
+                                song.lyricsUrl?.let { it to "text/plain" }
+                            },
+                        ).map { (inputUrl, contentType) ->
+                            val downloadUrl =
+                                if (inputUrl.startsWith("s3://")) {
+                                    val (bucket, key) = inputUrl.toBucketAndKey()
+                                    amazonS3.generatePresignedUrl(
+                                        bucket,
+                                        key,
+                                        Date.from(Instant.now().plus(30, ChronoUnit.MINUTES)),
+                                        HttpMethod.GET
+                                    ).toExternalForm()
+                                } else {
+                                    inputUrl
+                                }
+                            WeaveFile(
+                                url = downloadUrl,
+                                contentType = contentType,
+                            )
+                        },
+                    checkAndFund = false,
+                )
             )
         )
-    )
 }
