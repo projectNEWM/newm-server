35f9c38783f0982da24a5c9925012cc306c999f5
Andrew Westberg <andrewwestberg@gmail.com>
2024-04-30 17:53:39 +0000

    STUD-220: Fix aws Koin setup

--- newm-server/src/main/kotlin/io/newm/server/aws/AwsKoinModule.kt
+++ newm-server/src/main/kotlin/io/newm/server/aws/AwsKoinModule.kt
@@ -1,8 +1,5 @@
 package io.newm.server.aws
 
-import com.amazonaws.regions.Region
-import com.amazonaws.regions.RegionUtils
-import com.amazonaws.regions.Regions
 import com.amazonaws.services.kms.AWSKMSAsync
 import com.amazonaws.services.kms.AWSKMSAsyncClientBuilder
 import com.amazonaws.services.lambda.AWSLambdaAsync
@@ -13,42 +10,44 @@ import com.amazonaws.services.secretsmanager.AWSSecretsManagerAsync
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerAsyncClientBuilder
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
-import io.ktor.server.application.ApplicationEnvironment
+import io.ktor.server.application.*
 import io.newm.shared.ktx.getConfigString
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+
+val AWS_REGION = named("aws.region")
 
 val awsKoinModule =
     module {
-
         single<AmazonS3> {
             AmazonS3ClientBuilder.standard()
-                .withRegion(get<Regions>())
+                .withRegion(get<String>(AWS_REGION))
                 .build()
         }
 
         single<AmazonSQSAsync> {
             AmazonSQSAsyncClientBuilder.standard()
-                .withRegion(get<Regions>())
+                .withRegion(get<String>(AWS_REGION))
                 .build()
         }
 
         single<AWSKMSAsync> {
             AWSKMSAsyncClientBuilder.standard()
-                .withRegion(get<Regions>())
+                .withRegion(get<String>(AWS_REGION))
                 .build()
         }
 
         single<AWSSecretsManagerAsync> {
             AWSSecretsManagerAsyncClientBuilder.standard()
-                .withRegion(get<Regions>())
+                .withRegion(get<String>(AWS_REGION))
                 .build()
         }
 
         single<AWSLambdaAsync> {
             AWSLambdaAsyncClientBuilder.standard()
-                .withRegion(get<Regions>())
+                .withRegion(get<String>(AWS_REGION))
                 .build()
         }
 
-        single<Region> { RegionUtils.getRegion(get<ApplicationEnvironment>().getConfigString("aws.region")) }
+        single<String>(AWS_REGION) { get<ApplicationEnvironment>().getConfigString("aws.region") }
     }
