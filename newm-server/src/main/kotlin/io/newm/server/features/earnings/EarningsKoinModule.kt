1201787f0cb34aeb169faed4f7aa5c50a07b7f0b
Andrew Westberg <andrewwestberg@gmail.com>
2024-06-25 16:38:02 +0000

    Fix EarningsRepository DI

--- newm-server/src/main/kotlin/io/newm/server/features/earnings/EarningsKoinModule.kt
+++ newm-server/src/main/kotlin/io/newm/server/features/earnings/EarningsKoinModule.kt
@@ -1,6 +1,7 @@
 package io.newm.server.features.earnings
 
 import io.newm.server.features.earnings.daemon.MonitorClaimOrderSchedulerDaemon
+import io.newm.server.features.earnings.repo.EarningsRepository
 import io.newm.server.features.earnings.repo.EarningsRepositoryImpl
 import io.newm.shared.daemon.Daemon
 import org.koin.dsl.module
@@ -9,7 +10,7 @@ private val monitorClaimOrderSchedulerDaemon by lazy { MonitorClaimOrderSchedule
 
 val earningsKoinModule =
     module {
-        single { EarningsRepositoryImpl(get(), get(), get(), get(), get()) }
+        single<EarningsRepository> { EarningsRepositoryImpl(get(), get(), get(), get(), get()) }
         single<MonitorClaimOrderSchedulerDaemon> { monitorClaimOrderSchedulerDaemon }
         single<Daemon> { monitorClaimOrderSchedulerDaemon }
     }
