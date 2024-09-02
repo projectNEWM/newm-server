6f34e72b367c35eb62a734ba48965e5a1d179039
Andrew Westberg <andrewwestberg@gmail.com>
2024-09-02 20:24:27 +0000

    STUD-329: Fix Kogmios Certificate parsing

--- newm-shared/src/main/kotlin/io/newm/shared/serialization/UUIDSerializer.kt
+++ newm-shared/src/main/kotlin/io/newm/shared/serialization/UUIDSerializer.kt
@@ -8,7 +8,7 @@ import kotlinx.serialization.encoding.Encoder
 import java.util.UUID
 
 object UUIDSerializer : KSerializer<UUID> {
-    override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
+    override val descriptor = PrimitiveSerialDescriptor("java.util.UUID", PrimitiveKind.STRING)
 
     override fun deserialize(decoder: Decoder): UUID = UUID.fromString(decoder.decodeString())
 
