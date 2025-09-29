c9533770ba82368468c3bb2778acc5b3b3bb2c14
Andrew Westberg <andrewwestberg@gmail.com>
2025-09-29 20:03:11 +0000

    chore: add zookeeper curator config logging

--- newm-server/src/main/kotlin/io/newm/server/curator/CuratorInstall.kt
+++ newm-server/src/main/kotlin/io/newm/server/curator/CuratorInstall.kt
@@ -1,5 +1,6 @@
 package io.newm.server.curator
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.newm.server.curator.support.CuratorPlugin
@@ -7,15 +8,29 @@ import io.newm.shared.ktx.getBoolean
 import io.newm.shared.ktx.getConfigChild
 import io.newm.shared.ktx.getInt
 import io.newm.shared.ktx.getString
+import kotlin.io.path.Path
+import kotlin.io.path.readText
 import org.apache.curator.ensemble.fixed.FixedEnsembleProvider
 import org.apache.curator.retry.ExponentialBackoffRetry
 
 fun Application.installCurator() {
+    val log = KotlinLogging.logger { }
     val config = environment.getConfigChild("curator")
     if (config.getBoolean("enabled")) {
         install(CuratorPlugin) {
             ensembleProvider(
-                FixedEnsembleProvider(config.getString("connectionString"), false),
+                FixedEnsembleProvider(
+                    config.getString("connectionString").also { connectionString ->
+                        log.info {
+                            "Using Zookeeper connection string: '$connectionString', contents: ${
+                                Path(
+                                    connectionString
+                                ).readText()
+                            }"
+                        }
+                    },
+                    false
+                ),
             )
             retryPolicy(ExponentialBackoffRetry(config.getInt("baseSleepTime"), config.getInt("maxRetries")))
         }
