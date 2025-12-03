0e82fd4eaca83884024e855d49d5c74af4a0c3be
Walter Lara <walter.pdx@gmail.com>
2025-12-03 10:06:02 -0800

    Added error logs for PayPal API failures

--- newm-server/src/main/kotlin/io/newm/server/features/paypal/PayPalRoutes.kt
+++ newm-server/src/main/kotlin/io/newm/server/features/paypal/PayPalRoutes.kt
@@ -1,5 +1,6 @@
 package io.newm.server.features.paypal
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.auth.authenticate
 import io.ktor.server.request.receive
@@ -7,6 +8,7 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.route
 import io.newm.server.auth.jwt.AUTH_JWT
+import io.newm.server.features.paypal.model.MintingDistributionOrderRequest
 import io.newm.server.features.paypal.repo.PayPalRepository
 import io.newm.server.ktx.myUserId
 import io.newm.shared.koin.inject
@@ -14,15 +16,28 @@ import io.newm.shared.ktx.post
 
 fun Routing.createPayPalRoutes() {
     val repository: PayPalRepository by inject()
+    val logger = KotlinLogging.logger {}
 
     authenticate(AUTH_JWT) {
         route("v1/paypal/minting-distribution/orders") {
             post {
-                respond(HttpStatusCode.Created, repository.createMintingDistributionOrder(myUserId, receive()))
+                val request = receive<MintingDistributionOrderRequest>()
+                try {
+                    respond(HttpStatusCode.Created, repository.createMintingDistributionOrder(myUserId, request))
+                } catch (e: Exception) {
+                    logger.error(e) { "PayPal order creation failed for songId={${request.songId}}" }
+                    throw e
+                }
             }
             post("{orderId}/capture") {
-                repository.captureMintingDistributionOrder(parameters["orderId"]!!)
-                respond(HttpStatusCode.OK)
+                val orderId = parameters["orderId"]!!
+                try {
+                    repository.captureMintingDistributionOrder(orderId)
+                    respond(HttpStatusCode.OK)
+                } catch (e: Exception) {
+                    logger.error(e) { "PayPal order capture failed for orderId=$orderId" }
+                    throw e
+                }
             }
         }
     }
