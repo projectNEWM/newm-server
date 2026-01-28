03d1ad03d0bf0dd76ba705090e1fa1004daecc36
Walter Lara <walter.pdx@gmail.com>
2026-01-28 11:54:12 -0800

    feat: optimize GetNftSongs endpoint by fetching data in parallel (#719)

--- newm-server/src/main/kotlin/io/newm/server/features/nftsong/repo/NftSongRepositoryImpl.kt
+++ newm-server/src/main/kotlin/io/newm/server/features/nftsong/repo/NftSongRepositoryImpl.kt
@@ -7,24 +7,27 @@ import io.newm.server.features.ethereum.repo.EthereumRepository
 import io.newm.server.features.nftsong.model.NftChainMetadata
 import io.newm.server.features.nftsong.model.NftSong
 import io.newm.server.typealiases.UserId
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 internal class NftSongRepositoryImpl(
     private val cardanoRepository: CardanoRepository,
     private val ethereumRepository: EthereumRepository
 ) : NftSongRepository {
-    override suspend fun getNftSongs(userId: UserId): List<NftSong> {
-        val nftSongs = mutableListOf<NftSong>()
-
-        nftSongs += cardanoRepository
-            .getWalletNftSongs(userId, includeLegacy = true, useDripDropz = false)
-            .map { it.toNftSong() }
-
-        nftSongs += ethereumRepository
-            .getWalletNftSongs(userId)
-            .map { it.toNftSong() }
-
-        return nftSongs
-    }
+    override suspend fun getNftSongs(userId: UserId): List<NftSong> =
+        coroutineScope {
+            val cardanoJob = async {
+                cardanoRepository
+                    .getWalletNftSongs(userId, includeLegacy = true, useDripDropz = false)
+                    .map { it.toNftSong() }
+            }
+            val ethereumJob = async {
+                ethereumRepository
+                    .getWalletNftSongs(userId)
+                    .map { it.toNftSong() }
+            }
+            cardanoJob.await() + ethereumJob.await()
+        }
 
     private fun CardanoNftSong.toNftSong(): NftSong =
         NftSong(
